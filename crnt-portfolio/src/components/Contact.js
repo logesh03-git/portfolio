fe287d535f9339109e20e943deb9589f7bdb1775
logesh03-git <logeahlogesh06@gmail.com>
2024-07-16 23:38:02 -0400

    updated backend url

--- crnt-portfolio/src/components/Contact.js
+++ crnt-portfolio/src/components/Contact.js
@@ -30,7 +30,7 @@ const Contact = () => {
             setButtonText('sending');
         
             try {
-                let response = await fetch("http://localhost:5000/contact", {
+                let response = await fetch("https://portfolio-backend-vert-gamma.vercel.app/", {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json;charset=utf-8"
